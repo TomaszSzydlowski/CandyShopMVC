d88c34e112977aaa417fe787bc465e8496030535
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2020-01-11 20:34:46 +0100

    added Repository

---
+++ CandyShop/CandyShop/Models/ICandyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public interface ICandyRepository
+    {
+        IEnumerable<Candy> GetAllCand { get; }
+        IEnumerable<Candy> GetCandyOnSale { get; }
+        Candy GetCandyById(int candyId);
+    }
+}
